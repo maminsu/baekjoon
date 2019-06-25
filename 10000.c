de0631be686447a8c966f18d002c6919216db1ae
maminsu <akals060215@gmail.com>
2019-06-25 23:13:53 +0900

    10000.c

---
+++ 10000.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int input1, input2;
+    scanf("%d %d," &a, &b);
+    printf("%d", a, b")
+    return 0;
+}
--- 7287.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("4/akals06\n");
-    return 0;
-}
