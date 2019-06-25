ba09bf692f4bb7046f88f455e7c54303a127a2be
maminsu <akals060215@gmail.com>
2019-06-25 22:12:29 +0900

    10171.c

--- 10172.c
+++ 10172.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf(|\_/|
-|q p|   /}
-( 0 )"""\
-|"^"`    |
-||_/=\\__|;)
+    printf("|\\_/|\n");
+printf("|q p|   /}\n");
+printf("( 0 )\"\"\"\\\n");
+printf("|\"^\"`    |\n");
+printf("||_/=\\\\__|\n");
     return 0;
 }
