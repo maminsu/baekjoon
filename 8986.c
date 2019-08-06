136b74e27ae2fc4d6c810eac6690b26901a3ac2e
maminsu <akals060215@gmail.com>
2019-08-06 22:10:32 +0900

    8986.c

---
+++ 8986.c
@@ -0,0 +1,421 @@
+#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}#include<stdio.h>
+
+ 
+
+int main(int argc, char*argv[])
+
+{
+
+        int test;
+
+        char score[80];
+
+        int sum = 0;
+
+        int temp = 0;
+
+        scanf("%d", &test);
+
+        for (int i = 0; i < test; i++)
+
+        {
+
+                scanf("%s", score);
+
+                temp = 0;
+
+                for (int x = 0; score[x] != NULL; x++)
+
+                {
+
+                        if (score[x] == 'X')
+
+                        {
+
+                                temp = 0;
+
+                        }
+
+                        else
+
+                        {
+
+                                temp++;
+
+                        }
+
+                        sum += temp;
+
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}v
+                }
+
+ 
+
+                printf("%d\n", sum);
+
+                sum = 0;
+
+        }
+
+        return 0;
+
+}
